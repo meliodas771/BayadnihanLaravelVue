3fbf5dad8155e3d42c676a98839fe03b844654d5
Meliodas <meliodas.demonheir@gmail.com>
2025-12-10 00:46:47 +0800

    nothing happen

--- bayadnihan-vue/utils/api.js
+++ bayadnihan-vue/utils/api.js
@@ -2,7 +2,7 @@
 
 export const useAPI = () => {
   const config = useRuntimeConfig();
-  const API_BASE_URL = config.public.apiBaseUrl || 'https://46966ecf336448.lhr.life/api';
+  const API_BASE_URL = config.public.apiBaseUrl || 'http://127.0.0.1:8000/api';
 
   // Helper function to get auth token
   const getAuthToken = () => {
