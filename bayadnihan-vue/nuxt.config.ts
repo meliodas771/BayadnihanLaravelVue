646abef5011b5f703019479ad8b7af6fcabd94dd
Meliodas <meliodas.demonheir@gmail.com>
2025-12-10 00:22:13 +0800

    no commit

--- bayadnihan-vue/nuxt.config.ts
+++ bayadnihan-vue/nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   modules: ['@pinia/nuxt'],
   runtimeConfig: {
     public: {
-      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'https://8997d1c0667334.lhr.life/api'
+      apiBaseUrl: process.env.NUXT_PUBLIC_API_BASE_URL || 'https://46966ecf336448.lhr.life/api'
     }
   },
   app: {
